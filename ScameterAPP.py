615446db2571fa1d5bca8f50d5094171e24b49ad
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-05 22:19:19 +0800

    Update ScameterAPP.py

--- ScameterAPP.py
+++ ScameterAPP.py
@@ -13,6 +13,7 @@ def installff():
 _ = installff()
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
