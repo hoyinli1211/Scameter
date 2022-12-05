f8c9beef326bd0ef39f8c7bffe766fe4f7ce392a
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-05 18:53:49 +0800

    Update ScameterAPP.py

--- ScameterAPP.py
+++ ScameterAPP.py
@@ -5,6 +5,13 @@
 import streamlit as st
 import os, sys
 
+@st.experimental_singleton
+def installff():
+    os.system('sbase install geckodriver')
+    os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
