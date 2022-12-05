1c71d314a4ecb0e56cdcd911e360793996c15afe
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-05 22:12:57 +0800

    Update ScameterAPP.py

--- ScameterAPP.py
+++ ScameterAPP.py
@@ -5,6 +5,13 @@
 import streamlit as st
 import os, sys
 
+@st.experimental_singleton
+def installff():
+  os.system('sbase install chromedriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/chromedriver /home/appuser/venv/bin/chromedriver')
+
+_ = installff()
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
