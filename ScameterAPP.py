7483d6a42a1e1c5791c47e274df4ab536576c7b7
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-06 15:44:19 +0800

    Update ScameterAPP.py

--- ScameterAPP.py
+++ ScameterAPP.py
@@ -74,7 +74,7 @@ def scameterCheck(frame):
             driver.find_element(By.XPATH, '//*[@id="post-3646"]/div/div/section[2]/div/div/div/div/div/form/div/div/div[1]/div[2]/div[2]').click()
             time.sleep(5)
             
-            driver.set_window_size(1920, 1400)
+            driver.set_window_size(1920, 1300)
             driver.get_screenshot_as_file(JobID + ".png")
             image = Image.open(JobID + ".png")
             vImage = vImage.append(JobID + ".png")
