7f46ac2dc2ede66d0343323f6b1d2efe894c32d4
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-07 15:23:01 +0800

    Update ScameterAPP.py

--- ScameterAPP.py
+++ ScameterAPP.py
@@ -174,7 +174,7 @@ else:
 st.header('STEP 4. Export return result and audit log on screenshot after review')
 if st.session_state['ind1']==True and st.session_state['ind2']==True:
     st.download_button("Download Output",
-                        df.to_excel(index=False),
+                        df.to_excel(),
                         mime='xlsx') 
     st.write(st.session_state['df'])
     with open("output1.pdf", "rb") as pdf_file:
