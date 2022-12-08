51ff4d4b359a24467049aed5ea043f60bb4d3567
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-08 16:13:57 +0800

    Update ScameterAPP.py

--- ScameterAPP.py
+++ ScameterAPP.py
@@ -31,7 +31,7 @@ import zipfile
 from fpdf import FPDF
 
 #initialization of session state
-if 'indEnd' not in st.session_state or st.session_state['indEnd'] != True:
+if 'indEnd' not in st.session_state:
 #if 'ind0' not in st.session_state:
     st.session_state['ind0'] = False
 #if 'ind1' not in st.session_state:
@@ -206,9 +206,7 @@ else:
     st.write(st.session_state) 
 
 if st.button("Clear All"):    
-    st.session_state['ind0'] = False
-    st.session_state['ind1'] = False
-    st.session_state['ind2'] = False
+    del st.session_state['indEnd']
 else:
     pass
 #End of Script
